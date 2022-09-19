dd0b22e1ceeeabd96b82f527ad4302715b0ebbf8
oasworldido <113874196+oasworldido@users.noreply.github.com>
2022-09-19 16:09:39 +0800

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -34,10 +34,10 @@ const RESOURCES = {
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/loading.gif": "950265cdf65d152bf3c7d8bb87b2a117",
 "icons/logo.png": "6f283c04a641e912c013e4f7af67979a",
-"index.html": "62c64ef542f563073b3b9488f9212700",
-"/": "62c64ef542f563073b3b9488f9212700",
+"index.html": "861b1fd7067e20c28bc779502af07690",
+"/": "861b1fd7067e20c28bc779502af07690",
 "logo.png": "6f283c04a641e912c013e4f7af67979a",
-"main.dart.js": "f1f5732454b4f894022fe8f35ad3589f",
+"main.dart.js": "49ba6d8fa5885489bd0525895249a6af",
 "manifest.json": "dddf31a43c502bc2f0cd396990d78abb",
 "version.json": "2df2fecd4c6b17d5f3aa900da53738d0"
 };
