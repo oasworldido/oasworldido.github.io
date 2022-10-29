9bcc7e45df8367a6767d6f74f46395320cd96517
testCS888 <114760935+testCS888@users.noreply.github.com>
2022-10-30 01:11:27 +0800

    5646546

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -167,10 +167,10 @@ const RESOURCES = {
 "icons/loading.gif": "950265cdf65d152bf3c7d8bb87b2a117",
 "icons/logo.jpg": "0c95019134d8fd2304e70cdb3bc3c94d",
 "icons/logo.png": "6f283c04a641e912c013e4f7af67979a",
-"index.html": "4bb0ad74f45d101693bae8f330efd032",
-"/": "4bb0ad74f45d101693bae8f330efd032",
+"index.html": "b18ca2160f7dd0fa0b35c4289258ff28",
+"/": "b18ca2160f7dd0fa0b35c4289258ff28",
 "logo.png": "6f283c04a641e912c013e4f7af67979a",
-"main.dart.js": "cef87197dbdebb93f69bd170f76ae654",
+"main.dart.js": "91dd50026b73a203095c2cb2cbe98756",
 "manifest.json": "dddf31a43c502bc2f0cd396990d78abb",
 "version.json": "2df2fecd4c6b17d5f3aa900da53738d0"
 };
